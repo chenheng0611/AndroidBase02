a613d1c0d2f44d6d8195930e5a59aa99315ff172
xray <chenheng0611@163.com>
2017-06-04 10:47:17 +0800

    修改xxxbug

--- app/src/main/java/com/qianfeng/androidbase02/MainActivity.java
+++ app/src/main/java/com/qianfeng/androidbase02/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //这是新的代码。。。。。
+        //新代码2222222
     }
 }
